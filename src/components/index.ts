1334bad2a6f12ac5b5551a9df11c05150f39119e
pitossomo <pitossomo@gmail.com>
2023-03-09 06:03:42 -0300

    🧼chore: Export base components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Footer from "./Footer/Footer";
+import Form from "./Form/Form";
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+import Section from "./Section/Section";
+
+export { Header, Footer, Form, Main, Section };
