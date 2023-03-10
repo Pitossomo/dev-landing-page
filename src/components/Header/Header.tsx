0265d901f880f08adcfa39ce204bb4d511b83204
pitossomo <pitossomo@gmail.com>
2023-03-10 06:04:52 -0300

    🎨style(Header): Add basic style to component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,5 @@
-const Header = () => <header>Header</header>;
+import styles from "./Header.module.css";
+
+const Header = () => <header className={styles.header}>Header</header>;
 
 export default Header;
