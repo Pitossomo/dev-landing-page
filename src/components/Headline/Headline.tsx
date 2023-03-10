87592001607c6d4e710bf499af3e09eb333c2402
pitossomo <pitossomo@gmail.com>
2023-03-10 06:05:27 -0300

    ✨feat(Headline): Add Headline component

---
+++ src/components/Headline/Headline.tsx
@@ -0,0 +1,9 @@
+const Headline = () => (
+  <div>
+    <p>CUPOM DE DESCONTO</p>
+    <p>Vamos te presentear com um CUPOM de 30% 😍</p>
+    <p>Cadastre-se no formulário ao lado para receber seu conteúdo</p>
+  </div>
+);
+
+export default Headline;
