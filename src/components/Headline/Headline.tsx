84818d64dd056d9137a8c40b9542e6fd1c72a21d
pitossomo <pitossomo@gmail.com>
2023-03-13 06:11:04 -0300

    🎨style(Headline): Add class to Headline elements

--- src/components/Headline/Headline.tsx
+++ src/components/Headline/Headline.tsx
@@ -1,8 +1,16 @@
+import styles from "./Headline.module.css";
+
 const Headline = () => (
-  <div>
-    <p>CUPOM DE DESCONTO</p>
-    <p>Vamos te presentear com um CUPOM de 30% 😍</p>
-    <p>Cadastre-se no formulário ao lado para receber seu conteúdo</p>
+  <div className={styles.headline}>
+    <div className={styles.content}>
+      <p className={styles.title}>Cupom de desconto</p>
+      <p className={styles.subtitle}>
+        Vamos te presentear com um CUPOM de 30% 😍
+      </p>
+      <p className={styles.text}>
+        Cadastre-se no formulário ao lado para receber seu conteúdo
+      </p>
+    </div>
   </div>
 );
 
