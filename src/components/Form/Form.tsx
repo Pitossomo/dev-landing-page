bfa5b62446fd0b35aefe0cb17ac97bef54e59bc4
pitossomo <pitossomo@gmail.com>
2023-03-14 06:13:34 -0300

    ✨feat(Form): Add Inputs, title and button to Form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,15 @@
+import styles from "./Form.module.css";
+import { Input } from "..";
+
 const Form = () => (
-  <form>
-    <input></input>
+  <form className={styles.form}>
+    <h1 className={styles.title}>Cadastre-se e receba seu desconto</h1>
+    <Input label="Nome" />
+    <Input label="Email" />
+    <Input label="Estado" />
+    <Input label="Cidade" />
+    <Input label="6+11=?" isRequired={false} />
+    <button className={styles.button}>Quero meu cupom de 30%</button>
   </form>
 );
 
