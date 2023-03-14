a82f84db8e4a70cc02279edaeb0b9ac65e4e3c11
pitossomo <pitossomo@gmail.com>
2023-03-14 06:14:02 -0300

    🧼chore: Export new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
 import Footer from "./Footer/Footer";
 import Form from "./Form/Form";
 import Header from "./Header/Header";
+import Input from "./Input/Input";
 import Main from "./Main/Main";
 import Section from "./Section/Section";
+import Headline from "./Headline/Headline";
 
-export { Header, Footer, Form, Main, Section };
+export { Header, Footer, Form, Main, Section, Input, Headline };
